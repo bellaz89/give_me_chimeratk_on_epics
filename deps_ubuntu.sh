55433ddd8a5dda7132995bf69fe466ed15dd2084
Andrea Bellandi <andrea.bellandi@sckcen.be>
2026-02-13 14:40:28 +0100

    u?:nable to find ControlSystemAdapter-config? Where is build

---
+++ deps_ubuntu.sh
@@ -0,0 +1,11 @@
+sudo apt update; sudo apt upgrade -y
+sudo apt install cmake build-essential libreadline-dev -y
+sudo apt install libhdf5-dev
+sudo apt install autoconf automake libtool
+sudo apt install libboost-dev libboost-thread-dev libboost-filesystem-dev libboost-test-dev libboost-program-options-dev libboost-log-dev -y
+sudo apt install python3-dev python3-numpy python3-pip
+sudo apt install python3-pybind11 pybind11-dev mypy  -y
+sudo apt install libxml2-dev libxml++2.6-dev
+sudo apt install libfindbin-libs-perl
+sudo apt install qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5charts5-dev
+
