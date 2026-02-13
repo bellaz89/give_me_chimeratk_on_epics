f32090f7454a0bea9c0f1a89fcb4cd7f0784d11b
Andrea Bellandi <andrea.bellandi@sckcen.be>
2026-02-13 15:56:57 +0100

    feat: add deps_fedora

---
+++ deps_fedora.sh
@@ -0,0 +1,11 @@
+sudo dnf upgrade --refresh -y
+sudo dnf install @development-tools -y
+sudo dnf install cmake readline-devel -y
+sudo dnf install hdf5-devel -y
+sudo dnf install autoconf automake libtool -y
+sudo dnf install boost-devel boost-thread boost-filesystem boost-test boost-program-options boost-log -y
+sudo dnf install python3-devel python3-numpy python3-pip -y
+sudo dnf install python3-pybind11 pybind11-devel python3-mypy -y
+sudo dnf install libxml2-devel libxml++-devel -y
+sudo dnf install perl-FindBin -y
+sudo dnf install qt5-qtbase-devel qtchooser qt5-qttools-devel qt5-qtcharts-devel -y
