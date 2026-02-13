4913cd2ce321f908db6b43118c51d9b4d840010c
Andrea Bellandi <andrea.bellandi@sckcen.be>
2026-02-13 16:31:12 +0100

    Modified deps_fedora.sh

--- deps_fedora.sh
+++ deps_fedora.sh
@@ -8,4 +8,5 @@ sudo dnf install python3-devel python3-numpy python3-pip -y
 sudo dnf install python3-pybind11 pybind11-devel python3-mypy -y
 sudo dnf install libxml2-devel libxml++-devel -y
 sudo dnf install perl-FindBin -y
+sudo dnf install perl-FindBin perl-ExtUtils-MakeMaker -y
 sudo dnf install qt5-qtbase-devel qtchooser qt5-qttools-devel qt5-qtcharts-devel -y
